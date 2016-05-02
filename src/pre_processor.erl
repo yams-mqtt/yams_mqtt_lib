eca15f11b7ac43cd0fd77be69f45ba0887ca6be4
KuldeepSinh <chauhan.kuldeep.sinh@gmail.com>
2016-05-01 22:59:41 -0700

    Rename functions
    What?
    Renamed all the functions starting with 'compile'
    
    Why?
    The functions are more or less behave like an interpreter,
    rather than a compiler. As it doesn't parse entire message at one go.
    Instead, it parses message token by token. If anywhere it finds invalid
    token, it does not process rest of the message.

--- src/pre_processor.erl
+++ src/pre_processor.erl
@@ -1,27 +1,27 @@
 %%%-------------------------------------------------------------------
 %%% @author kuldeep 
 %%% @copyright (C) 2016, kuldeep
-%%% @doc
+%%% @doc 
 %%%
 %%% @end Created : 13 April 2016 by kuldeep
 %%% -------------------------------------------------------------------
 -module(pre_processor).
 -include("../include/yams_lib.hrl").
 %% API
--export([compile_packet/1]).
+-export([interprete_packet/1]).
 %% Macro
 -define(MAX_LENGTH, 268435455). % Maximum allowed length of the topic.
 
 %%===================================================================
--spec compile_packet(packet_binary()) -> 
+-spec interprete_packet(packet_binary()) -> 
 			    remaining_length_ok() 
 			   |remaining_length_error()
 			   |packet_type_error().
 				
-compile_packet(Binary) ->
-    compile_remaining_length(compile_packet_type(Binary)).
+interprete_packet(Binary) ->
+    interprete_remaining_length(interprete_packet_type(Binary)).
 %%===================================================================
-%% Roughly speaking, compile_packet_type/1 behaves like a compiler-front-end.
+%% Roughly speaking, interprete_packet_type/1 behaves like an interpreter-front-end.
 %% It scans (creates tokens from the) first byte of the control packet. 
 %% And parses (analyzes) the tokens and determines their validity.
 %% As an output the function returns following.
@@ -41,43 +41,43 @@ compile_packet(Binary) ->
 			     ,binary()}.
 
 
--spec compile_packet_type(packet_binary()) -> 
+-spec interprete_packet_type(packet_binary()) -> 
 				 packet_type_ok()
 				|packet_type_error().
 
-compile_packet_type(<<1:4, 0:1, 0:2, 0:1, RemainingBin/binary>>) ->
+interprete_packet_type(<<1:4, 0:1, 0:2, 0:1, RemainingBin/binary>>) ->
     {ok, #packet_type{msgtype = connect, dup = 0, qos = 0, retain = 0}, RemainingBin};
-compile_packet_type(<<2:4, 0:1, 0:2, 0:1, RemainingBin/binary>>) ->
+interprete_packet_type(<<2:4, 0:1, 0:2, 0:1, RemainingBin/binary>>) ->
     {ok, #packet_type{msgtype = connack, dup = 0, qos = 0, retain = 0}, RemainingBin};
-compile_packet_type(<<3:4, Dup:1, 0:2, Retain:1, RemainingBin/binary>>) ->
+interprete_packet_type(<<3:4, Dup:1, 0:2, Retain:1, RemainingBin/binary>>) ->
     {ok, #packet_type{msgtype = publish, dup = Dup, qos = 0, retain = Retain}, RemainingBin};
-compile_packet_type(<<3:4, Dup:1, 1:2, Retain:1, RemainingBin/binary>>) ->
+interprete_packet_type(<<3:4, Dup:1, 1:2, Retain:1, RemainingBin/binary>>) ->
     {ok, #packet_type{msgtype = publish, dup = Dup, qos = 1, retain = Retain}, RemainingBin};
-compile_packet_type(<<3:4, Dup:1, 2:2, Retain:1, RemainingBin/binary>>) ->
+interprete_packet_type(<<3:4, Dup:1, 2:2, Retain:1, RemainingBin/binary>>) ->
     {ok, #packet_type{msgtype = publish, dup = Dup, qos = 2, retain = Retain}, RemainingBin};
-compile_packet_type(<<4:4, 0:1, 0:2, 0:1, RemainingBin/binary>>) ->
+interprete_packet_type(<<4:4, 0:1, 0:2, 0:1, RemainingBin/binary>>) ->
     {ok, #packet_type{msgtype = puback, dup = 0, qos = 0, retain = 0}, RemainingBin};
-compile_packet_type(<<5:4, 0:1, 0:2, 0:1, RemainingBin/binary>>) ->
+interprete_packet_type(<<5:4, 0:1, 0:2, 0:1, RemainingBin/binary>>) ->
     {ok, #packet_type{msgtype = pubrec, dup = 0, qos = 0, retain = 0}, RemainingBin};
-compile_packet_type(<<6:4, 0:1, 1:2, 0:1, RemainingBin/binary>>) ->
+interprete_packet_type(<<6:4, 0:1, 1:2, 0:1, RemainingBin/binary>>) ->
     {ok, #packet_type{msgtype = pubrel, dup = 0, qos = 1, retain = 0}, RemainingBin};
-compile_packet_type(<<7:4, 0:1, 0:2, 0:1, RemainingBin/binary>>) ->
+interprete_packet_type(<<7:4, 0:1, 0:2, 0:1, RemainingBin/binary>>) ->
     {ok, #packet_type{msgtype = pubcomp, dup = 0, qos = 0, retain = 0}, RemainingBin};
-compile_packet_type(<<8:4, 0:1, 1:2, 0:1, RemainingBin/binary>>) ->
+interprete_packet_type(<<8:4, 0:1, 1:2, 0:1, RemainingBin/binary>>) ->
     {ok, #packet_type{msgtype = subscribe, dup = 0, qos = 1, retain = 0}, RemainingBin};
-compile_packet_type(<<9:4, 0:1, 0:2, 0:1, RemainingBin/binary>>) ->
+interprete_packet_type(<<9:4, 0:1, 0:2, 0:1, RemainingBin/binary>>) ->
     {ok, #packet_type{msgtype = suback, dup = 0, qos = 0, retain = 0}, RemainingBin};
-compile_packet_type(<<10:4, 0:1, 1:2, 0:1, RemainingBin/binary>>) ->
+interprete_packet_type(<<10:4, 0:1, 1:2, 0:1, RemainingBin/binary>>) ->
     {ok, #packet_type{msgtype = unsubscribe, dup = 0, qos = 1, retain = 0}, RemainingBin};
-compile_packet_type(<<11:4, 0:1, 0:2, 0:1, RemainingBin/binary>>) ->
+interprete_packet_type(<<11:4, 0:1, 0:2, 0:1, RemainingBin/binary>>) ->
     {ok, #packet_type{msgtype = unsuback, dup = 0, qos = 0, retain = 0}, RemainingBin};
-compile_packet_type(<<12:4, 0:1, 0:2, 0:1, RemainingBin/binary>>) ->
+interprete_packet_type(<<12:4, 0:1, 0:2, 0:1, RemainingBin/binary>>) ->
     {ok, #packet_type{msgtype = pingreq, dup = 0, qos = 0, retain = 0}, RemainingBin};
-compile_packet_type(<<13:4, 0:1, 0:2, 0:1, RemainingBin/binary>>) ->
+interprete_packet_type(<<13:4, 0:1, 0:2, 0:1, RemainingBin/binary>>) ->
     {ok, #packet_type{msgtype = pingresp, dup = 0, qos = 0, retain = 0}, RemainingBin};
-compile_packet_type(<<14:4, 0:1, 0:2, 0:1, RemainingBin/binary>>) ->
+interprete_packet_type(<<14:4, 0:1, 0:2, 0:1, RemainingBin/binary>>) ->
     {ok, #packet_type{msgtype = disconnect, dup = 0, qos = 0, retain = 0}, RemainingBin};
-compile_packet_type(OtherBin) ->
+interprete_packet_type(OtherBin) ->
     {error, invalid_fb, OtherBin}.
     
 %%===================================================================
@@ -85,14 +85,14 @@ compile_packet_type(OtherBin) ->
 %% 1. Remaining length of the packet, which I call it - var_load
 %% 2. var_load (= variable header + payload)
 %%===================================================================
--spec compile_remaining_length(packet_type_ok()
+-spec interprete_remaining_length(packet_type_ok()
 			      |packet_type_error())->
 				      remaining_length_ok() 
 				     |remaining_length_error()
 				     |packet_type_error().
-compile_remaining_length({ok, PacketType, RemainingBin}) ->
-    compile_remaining_length({PacketType, RemainingBin}, 0, 1);
-compile_remaining_length({error, invalid_fb, OtherBin}) ->
+interprete_remaining_length({ok, PacketType, RemainingBin}) ->
+    interprete_remaining_length({PacketType, RemainingBin}, 0, 1);
+interprete_remaining_length({error, invalid_fb, OtherBin}) ->
     {error, invalid_fb, OtherBin}.
 
 %%==============================================
@@ -112,20 +112,20 @@ compile_remaining_length({error, invalid_fb, OtherBin}) ->
 				  ,remaining_length()
 				  ,binary()}.
 
--spec compile_remaining_length({packet_type(),binary()},non_neg_integer(),pos_integer()) -> 
+-spec interprete_remaining_length({packet_type(),binary()},non_neg_integer(),pos_integer()) -> 
 				      remaining_length_ok()
 				     |remaining_length_error().
 				    
-compile_remaining_length(TypeCompiledPacket, _RemainingLength, Multiplier)
+interprete_remaining_length(TypeInterpretedPacket, _RemainingLength, Multiplier)
   when (Multiplier > 128 * 128 * 128) ->
-    {error, remaining_length_exceeds_max_length, TypeCompiledPacket};
+    {error, remaining_length_exceeds_max_length, TypeInterpretedPacket};
 %% Calculate the remaining length value:
 %% Recurse if the value of the first bit is 1.
-compile_remaining_length({PacketType, <<1:1, Len:7, Rest/binary>>}, RemainingLength, Multiplier) ->
-    compile_remaining_length({PacketType, Rest}, RemainingLength + Len * Multiplier, Multiplier * 128);
+interprete_remaining_length({PacketType, <<1:1, Len:7, Rest/binary>>}, RemainingLength, Multiplier) ->
+    interprete_remaining_length({PacketType, Rest}, RemainingLength + Len * Multiplier, Multiplier * 128);
 %% Calculate Value of the remaining length :
 %% Return if the value of the first bit is 0.
-compile_remaining_length({PacketType, <<0:1, Len:7, VarLoad/binary>>}, RemainingLength, Multiplier) ->
+interprete_remaining_length({PacketType, <<0:1, Len:7, VarLoad/binary>>}, RemainingLength, Multiplier) ->
     RemLen = RemainingLength + (Len * Multiplier),
     case (RemLen =:= size(VarLoad)) of
 	true -> 
