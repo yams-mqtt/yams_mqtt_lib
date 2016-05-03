c13e920752df425b9a03bbd5d9812cfddd3d10bb
KuldeepSinh <chauhan.kuldeep.sinh@gmail.com>
2016-05-02 22:49:56 -0700

    Update type specifications

--- test/pre_processor_test.erl
+++ test/pre_processor_test.erl
@@ -16,93 +16,93 @@
 
 %% CONNECT Test
 get_msg_type_for_connect_test() ->
-    ?assertEqual({ok, #packet_type{msgtype = connect, dup = 0, qos = 0, retain = 0}, <<100>>},
-		  (pre_processor:interprete_packet_type(<<1:4, 0:1, 0:2, 0:1, 100:8>>))).
+    ?assertEqual({ok, #type_byte{msgtype = connect, dup = 0, qos = 0, retain = 0}, <<100>>},
+		  (pre_processor:interprete_type_byte(<<1:4, 0:1, 0:2, 0:1, 100:8>>))).
 
 %% CONNACK Test
 get_msg_type_for_connack_test() ->
-    ?assertEqual({ok, #packet_type{msgtype = connack, dup = 0, qos = 0, retain = 0}, <<2>>},
-		  (pre_processor:interprete_packet_type(<<2:4, 0:1, 0:2, 0:1, 2:8>>))).
+    ?assertEqual({ok, #type_byte{msgtype = connack, dup = 0, qos = 0, retain = 0}, <<2>>},
+		  (pre_processor:interprete_type_byte(<<2:4, 0:1, 0:2, 0:1, 2:8>>))).
 
 %% PUBLISH Test
 get_msg_type_for_publish_test() ->
-    ?assertEqual({ok, #packet_type{msgtype = publish, dup = 0, qos = 0, retain = 0}, <<100>>},
-		  (pre_processor:interprete_packet_type(<<3:4, 0:1, 0:2, 0:1, 100:8>>))).
+    ?assertEqual({ok, #type_byte{msgtype = publish, dup = 0, qos = 0, retain = 0}, <<100>>},
+		  (pre_processor:interprete_type_byte(<<3:4, 0:1, 0:2, 0:1, 100:8>>))).
 
 %% PUBLISH Test with DUP flag = 1
 get_msg_type_for_publish_with_dup_flag_value_1_test() ->
-    ?assertEqual({ok, #packet_type{msgtype = publish, dup = 1, qos = 0, retain = 0}, <<100>>},
-		  (pre_processor:interprete_packet_type(<<3:4, 1:1, 0:2, 0:1, 100:8>>))).
+    ?assertEqual({ok, #type_byte{msgtype = publish, dup = 1, qos = 0, retain = 0}, <<100>>},
+		  (pre_processor:interprete_type_byte(<<3:4, 1:1, 0:2, 0:1, 100:8>>))).
 
 %% PUBLISH Test with Qos flag = 1
 get_msg_type_for_publish_with_qos_flag_value_1_test() ->
-    ?assertEqual({ok, #packet_type{msgtype = publish, dup = 1, qos = 1, retain = 0}, <<100>>},
-		  (pre_processor:interprete_packet_type(<<3:4, 1:1, 1:2, 0:1, 100:8>>))).
+    ?assertEqual({ok, #type_byte{msgtype = publish, dup = 1, qos = 1, retain = 0}, <<100>>},
+		  (pre_processor:interprete_type_byte(<<3:4, 1:1, 1:2, 0:1, 100:8>>))).
 
 %% PUBLISH Test with Qos flag = 2
 get_msg_type_for_publish_with_qos_flag_value_2_test() ->
-    ?assertEqual({ok, #packet_type{msgtype = publish, dup = 0, qos = 2, retain = 0}, <<100>>},
-		  (pre_processor:interprete_packet_type(<<3:4, 0:1, 2:2, 0:1, 100:8>>))).
+    ?assertEqual({ok, #type_byte{msgtype = publish, dup = 0, qos = 2, retain = 0}, <<100>>},
+		  (pre_processor:interprete_type_byte(<<3:4, 0:1, 2:2, 0:1, 100:8>>))).
 
 %% PUBLISH Test with Retain flag = 1
 get_msg_type_for_publish_with_retain_flag_value_1_test() ->
-    ?assertEqual({ok, #packet_type{msgtype = publish, dup = 0, qos = 2, retain = 1}, <<100>>},
-		  (pre_processor:interprete_packet_type(<<3:4, 0:1, 2:2, 1:1, 100:8>>))).
+    ?assertEqual({ok, #type_byte{msgtype = publish, dup = 0, qos = 2, retain = 1}, <<100>>},
+		  (pre_processor:interprete_type_byte(<<3:4, 0:1, 2:2, 1:1, 100:8>>))).
 
 %% PUBACK Test
 get_msg_type_for_puback_test() ->
-    ?assertEqual({ok, #packet_type{msgtype = puback, dup = 0, qos = 0, retain = 0}, <<2>>},
-		  (pre_processor:interprete_packet_type(<<4:4, 0:1, 0:2, 0:1, 2:8>>))).
+    ?assertEqual({ok, #type_byte{msgtype = puback, dup = 0, qos = 0, retain = 0}, <<2>>},
+		  (pre_processor:interprete_type_byte(<<4:4, 0:1, 0:2, 0:1, 2:8>>))).
 
 %% PUBREC Test
 get_msg_type_for_pubrec_test() ->
-    ?assertEqual({ok, #packet_type{msgtype = pubrec, dup = 0, qos = 0, retain = 0}, <<2>>},
-		  (pre_processor:interprete_packet_type(<<5:4, 0:1, 0:2, 0:1, 2:8>>))).
+    ?assertEqual({ok, #type_byte{msgtype = pubrec, dup = 0, qos = 0, retain = 0}, <<2>>},
+		  (pre_processor:interprete_type_byte(<<5:4, 0:1, 0:2, 0:1, 2:8>>))).
 
 %% PUBREL Test
 get_msg_type_for_pubrel_test() ->
-    ?assertEqual({ok, #packet_type{msgtype = pubrel, dup = 0, qos = 1, retain = 0}, <<2>>},
-		  (pre_processor:interprete_packet_type(<<6:4, 0:1, 1:2, 0:1, 2:8>>))).
+    ?assertEqual({ok, #type_byte{msgtype = pubrel, dup = 0, qos = 1, retain = 0}, <<2>>},
+		  (pre_processor:interprete_type_byte(<<6:4, 0:1, 1:2, 0:1, 2:8>>))).
 
 %% PUBCOMP Test
 get_msg_type_for_pubcomp_test() ->
-    ?assertEqual({ok, #packet_type{msgtype = pubcomp, dup = 0, qos = 0, retain = 0}, <<2>>},
-		  (pre_processor:interprete_packet_type(<<7:4, 0:1, 0:2, 0:1, 2:8>>))).
+    ?assertEqual({ok, #type_byte{msgtype = pubcomp, dup = 0, qos = 0, retain = 0}, <<2>>},
+		  (pre_processor:interprete_type_byte(<<7:4, 0:1, 0:2, 0:1, 2:8>>))).
 
 %% SUBSCRIBE Test
 get_msg_type_for_subscribe_test() ->
-    ?assertEqual({ok, #packet_type{msgtype = subscribe, dup = 0, qos = 1, retain = 0}, <<100>>},
-		  (pre_processor:interprete_packet_type(<<8:4, 0:1, 1:2, 0:1, 100:8>>))).
+    ?assertEqual({ok, #type_byte{msgtype = subscribe, dup = 0, qos = 1, retain = 0}, <<100>>},
+		  (pre_processor:interprete_type_byte(<<8:4, 0:1, 1:2, 0:1, 100:8>>))).
 
 %% SUBACK Test
 get_msg_type_for_suback_test() ->
-    ?assertEqual({ok, #packet_type{msgtype = suback, dup = 0, qos = 0, retain = 0}, <<100>>},
-		  (pre_processor:interprete_packet_type(<<9:4, 0:1, 0:2, 0:1, 100:8>>))).
+    ?assertEqual({ok, #type_byte{msgtype = suback, dup = 0, qos = 0, retain = 0}, <<100>>},
+		  (pre_processor:interprete_type_byte(<<9:4, 0:1, 0:2, 0:1, 100:8>>))).
 
 %% UNSUBSCRIBE Test
 get_msg_type_for_unsubscribe_test() ->
-    ?assertEqual({ok, #packet_type{msgtype = unsubscribe, dup = 0, qos = 1, retain = 0}, <<100>>},
-		  (pre_processor:interprete_packet_type(<<10:4, 0:1, 1:2, 0:1, 100:8>>))).
+    ?assertEqual({ok, #type_byte{msgtype = unsubscribe, dup = 0, qos = 1, retain = 0}, <<100>>},
+		  (pre_processor:interprete_type_byte(<<10:4, 0:1, 1:2, 0:1, 100:8>>))).
 
 %% UNSUBACK Test
 get_msg_type_for_unsuback_test() ->
-    ?assertEqual({ok, #packet_type{msgtype = unsuback, dup = 0, qos = 0, retain = 0}, <<2>>},
-		  (pre_processor:interprete_packet_type(<<11:4, 0:1, 0:2, 0:1, 2:8>>))).
+    ?assertEqual({ok, #type_byte{msgtype = unsuback, dup = 0, qos = 0, retain = 0}, <<2>>},
+		  (pre_processor:interprete_type_byte(<<11:4, 0:1, 0:2, 0:1, 2:8>>))).
 
 %% PINGREQ Test
 get_msg_type_for_pingreq_test() ->
-    ?assertEqual({ok, #packet_type{msgtype = pingreq, dup = 0, qos = 0, retain = 0}, <<0>>},
-		  (pre_processor:interprete_packet_type(<<12:4, 0:1, 0:2, 0:1, 0:8>>))).
+    ?assertEqual({ok, #type_byte{msgtype = pingreq, dup = 0, qos = 0, retain = 0}, <<0>>},
+		  (pre_processor:interprete_type_byte(<<12:4, 0:1, 0:2, 0:1, 0:8>>))).
 
 %% pingresp Test
 get_msg_type_for_pingresp_test() ->
-    ?assertEqual({ok, #packet_type{msgtype = pingresp, dup = 0, qos = 0, retain = 0}, <<0>>},
-		  (pre_processor:interprete_packet_type(<<13:4, 0:1, 0:2, 0:1, 0:8>>))).
+    ?assertEqual({ok, #type_byte{msgtype = pingresp, dup = 0, qos = 0, retain = 0}, <<0>>},
+		  (pre_processor:interprete_type_byte(<<13:4, 0:1, 0:2, 0:1, 0:8>>))).
 
 %% DISCONNECT Test
 get_msg_type_for_disconnect_test() ->
-    ?assertEqual({ok, #packet_type{msgtype = disconnect, dup = 0, qos = 0, retain = 0}, <<0>>},
-		  (pre_processor:interprete_packet_type(<<14:4, 0:1, 0:2, 0:1, 0:8>>))).
+    ?assertEqual({ok, #type_byte{msgtype = disconnect, dup = 0, qos = 0, retain = 0}, <<0>>},
+		  (pre_processor:interprete_type_byte(<<14:4, 0:1, 0:2, 0:1, 0:8>>))).
 
 %% Test Invalid values
 get_test_data() ->
@@ -202,10 +202,10 @@ get_test_data() ->
 	      , #testdata{msgtype = 14, dup = 0, qos = 0, retain = 1}
 	      ].
 
-invalid_packet_type_test_() ->
+invalid_type_byte_test_() ->
     TestData = get_test_data(),
     [?_assertEqual({error, invalid_fb, <<MsgType:4, Dup:1, QoS:2, Retain:1, 100:8>>}, 
-		  (pre_processor:interprete_packet_type(<<MsgType:4, Dup:1, QoS:2, Retain:1, 100:8>>)))
+		  (pre_processor:interprete_type_byte(<<MsgType:4, Dup:1, QoS:2, Retain:1, 100:8>>)))
      || #testdata{msgtype = MsgType, dup = Dup, qos = QoS, retain = Retain} <- TestData ].
 
 %%==========================================================================
@@ -213,58 +213,58 @@ invalid_packet_type_test_() ->
 interprete_remaining_length_when_packetype_has_error_test_() ->
     TestData = get_test_data(),
     [?_assertEqual({error, invalid_fb, <<MsgType:4, Dup:1, QoS:2, Retain:1, 100:8>>}, 
-		  (pre_processor:interprete_remaining_length(pre_processor:interprete_packet_type(<<MsgType:4, Dup:1, QoS:2, Retain:1, 100:8>>))))
+		  (pre_processor:interprete_remaining_length(pre_processor:interprete_type_byte(<<MsgType:4, Dup:1, QoS:2, Retain:1, 100:8>>))))
      || #testdata{msgtype = MsgType, dup = Dup, qos = QoS, retain = Retain} <- TestData ].
 
 %% interprete_remaining_length_test - when remaining binary is too short
 interprete_remaining_length_when_remaining_binary_is_too_short_test() ->
-    ?assertEqual({error, remaining_length_differs_actual_length, {packet_type,connect,0,0,0}, 1, <<>>},
-		 (pre_processor:interprete_remaining_length(pre_processor:interprete_packet_type(<<1:4, 0:4, 1:8>>)))).
+    ?assertEqual({error, remaining_length_differs_actual_length, {type_byte,connect,0,0,0}, 1, <<>>},
+		 (pre_processor:interprete_remaining_length(pre_processor:interprete_type_byte(<<1:4, 0:4, 1:8>>)))).
 
 %% interprete_remaining_length_test - when remaining binary is too long
 interprete_remaining_length_when_remaining_binary_is_too_long_test() ->
-    ?assertEqual({error, remaining_length_differs_actual_length, {packet_type,connect,0,0,0}, 1, <<100:16>>},
-		 (pre_processor:interprete_remaining_length(pre_processor:interprete_packet_type(<<1:4, 0:4, 1:8, 100:16>>)))).
+    ?assertEqual({error, remaining_length_differs_actual_length, {type_byte,connect,0,0,0}, 1, <<100:16>>},
+		 (pre_processor:interprete_remaining_length(pre_processor:interprete_type_byte(<<1:4, 0:4, 1:8, 100:16>>)))).
 
 %% interprete_remaining_length_test - lower limit of the first byte of the remaining length.
 interprete_remaining_length_0_test() ->
-    ?assertEqual({ok, #packet_type{msgtype = connect, dup =0, qos = 0, retain = 0}, 0, <<>>},
-		 (pre_processor:interprete_remaining_length(pre_processor:interprete_packet_type(<<1:4, 0:4, 0:8>>)))).
+    ?assertEqual({ok, #type_byte{msgtype = connect, dup =0, qos = 0, retain = 0}, 0, <<>>},
+		 (pre_processor:interprete_remaining_length(pre_processor:interprete_type_byte(<<1:4, 0:4, 0:8>>)))).
 
 %% interprete_remaining_length_test - where remaining length is 2 bytes.
 interprete_remaining_length_1_test() ->
-    ?assertEqual({ok, #packet_type{msgtype = connect, dup =0, qos = 0, retain = 0}, 2, <<100:16>>},
-		 (pre_processor:interprete_remaining_length(pre_processor:interprete_packet_type(<<1:4, 0:4, 2:8, 100:16>>)))).
+    ?assertEqual({ok, #type_byte{msgtype = connect, dup =0, qos = 0, retain = 0}, 2, <<100:16>>},
+		 (pre_processor:interprete_remaining_length(pre_processor:interprete_type_byte(<<1:4, 0:4, 2:8, 100:16>>)))).
 %% interprete_remaining_length_test - upper limit of the first byte of the remaining length.
 interprete_remaining_length_127_test() ->
-    ?assertEqual({ok, #packet_type{msgtype = connect, dup =0, qos = 0, retain = 0}, 127, <<100:1016>>},
-		 (pre_processor:interprete_remaining_length(pre_processor:interprete_packet_type(<<1:4, 0:4, 127:8, 100:1016>>)))).
+    ?assertEqual({ok, #type_byte{msgtype = connect, dup =0, qos = 0, retain = 0}, 127, <<100:1016>>},
+		 (pre_processor:interprete_remaining_length(pre_processor:interprete_type_byte(<<1:4, 0:4, 127:8, 100:1016>>)))).
 
 %% interprete_remaining_length_test - lower limit of the second byte of the remaining length.
 interprete_remaining_length_128_test() ->
-    ?assertEqual({ok, #packet_type{msgtype = connect, dup =0, qos = 0, retain = 0}, 128, <<100:1024>>},
-		 (pre_processor:interprete_remaining_length(pre_processor:interprete_packet_type(<<1:4, 0:4, 128:8, 1:8, 100:1024>>)))).
+    ?assertEqual({ok, #type_byte{msgtype = connect, dup =0, qos = 0, retain = 0}, 128, <<100:1024>>},
+		 (pre_processor:interprete_remaining_length(pre_processor:interprete_type_byte(<<1:4, 0:4, 128:8, 1:8, 100:1024>>)))).
 
 %% interprete_remaining_length_test - upper limit of the second byte of the remaining length.
 interprete_remaining_length_16383_test() ->
-    ?assertEqual({ok, #packet_type{msgtype = connect, dup =0, qos = 0, retain = 0}, 16383, <<100:131064>>},
-		 (pre_processor:interprete_remaining_length(pre_processor:interprete_packet_type(<<1:4, 0:4, 255:8, 127:8, 100:131064>>)))).
+    ?assertEqual({ok, #type_byte{msgtype = connect, dup =0, qos = 0, retain = 0}, 16383, <<100:131064>>},
+		 (pre_processor:interprete_remaining_length(pre_processor:interprete_type_byte(<<1:4, 0:4, 255:8, 127:8, 100:131064>>)))).
 
 %% interprete_remaining_length_test - lower limit of the third byte of the remaining length.
 interprete_remaining_length_16384_test() ->
-    ?assertEqual({ok, #packet_type{msgtype = connect, dup =0, qos = 0, retain = 0}, 16384, <<100:131072>>},
-		 (pre_processor:interprete_remaining_length(pre_processor:interprete_packet_type(<<1:4, 0:4, 128:8, 128:8, 1:8, 100:131072>>)))).
+    ?assertEqual({ok, #type_byte{msgtype = connect, dup =0, qos = 0, retain = 0}, 16384, <<100:131072>>},
+		 (pre_processor:interprete_remaining_length(pre_processor:interprete_type_byte(<<1:4, 0:4, 128:8, 128:8, 1:8, 100:131072>>)))).
 
 
 %% interprete_remaining_length_test - upper limit of the third byte of the remaining length.
 interprete_remaining_length_2097151_test() ->
-    ?assertEqual({ok, #packet_type{msgtype = connect, dup =0, qos = 0, retain = 0}, 2097151, <<100:16777208>>},
-		 (pre_processor:interprete_remaining_length(pre_processor:interprete_packet_type(<<1:4, 0:4, 255:8, 255:8, 127:8, 100:16777208>>)))).
+    ?assertEqual({ok, #type_byte{msgtype = connect, dup =0, qos = 0, retain = 0}, 2097151, <<100:16777208>>},
+		 (pre_processor:interprete_remaining_length(pre_processor:interprete_type_byte(<<1:4, 0:4, 255:8, 255:8, 127:8, 100:16777208>>)))).
 
 %% interprete_remaining_length_test - lower limit of the fourth byte of the remaining length.
 interprete_remaining_length_2097152_test() ->
-    ?assertEqual({ok, #packet_type{msgtype = connect, dup =0, qos = 0, retain = 0}, 2097152, <<100:16777216>>},
-		 (pre_processor:interprete_remaining_length(pre_processor:interprete_packet_type(<<1:4, 0:4, 128:8, 128:8, 128:8, 1:8, 100:16777216>>)))).
+    ?assertEqual({ok, #type_byte{msgtype = connect, dup =0, qos = 0, retain = 0}, 2097152, <<100:16777216>>},
+		 (pre_processor:interprete_remaining_length(pre_processor:interprete_type_byte(<<1:4, 0:4, 128:8, 128:8, 128:8, 1:8, 100:16777216>>)))).
 
 %%=========================
 %% Note : Following 2 tests are long running tests.
@@ -274,13 +274,13 @@ interprete_remaining_length_2097152_test() ->
 
 %% %% interprete_remaining_length_test - upper limit of the fourth byte of the remaining length.
 interprete_remaining_length_268435455_test() ->
-    ?assertEqual({ok, #packet_type{msgtype = connect, dup =0, qos = 0, retain = 0}, 268435455, <<100:2147483640>>},
-		 (pre_processor:interprete_remaining_length(pre_processor:interprete_packet_type(<<1:4, 0:4, 255:8, 255:8, 255:8, 127:8, 100:2147483640>>)))).
+    ?assertEqual({ok, #type_byte{msgtype = connect, dup =0, qos = 0, retain = 0}, 268435455, <<100:2147483640>>},
+		 (pre_processor:interprete_remaining_length(pre_processor:interprete_type_byte(<<1:4, 0:4, 255:8, 255:8, 255:8, 127:8, 100:2147483640>>)))).
 
 %% %% interprete_remaining_length_test - exceeding the upper limit of the fourth byte of the remaining length.
 interprete_remaining_length_268435456_test() ->
-    ?assertEqual({error, remaining_length_exceeds_max_length, {#packet_type{msgtype = connect, dup =0, qos = 0, retain = 0}, <<1:8, 100:2147483648>>}},
-		 (pre_processor:interprete_remaining_length(pre_processor:interprete_packet_type(<<1:4, 0:4, 128:8, 128:8, 128:8, 128:8, 1:8, 100:2147483648>>)))).
+    ?assertEqual({error, remaining_length_exceeds_max_length, {#type_byte{msgtype = connect, dup =0, qos = 0, retain = 0}, <<1:8, 100:2147483648>>}},
+		 (pre_processor:interprete_remaining_length(pre_processor:interprete_type_byte(<<1:4, 0:4, 128:8, 128:8, 128:8, 128:8, 1:8, 100:2147483648>>)))).
 
 %%=========================
 %% interprete packet
@@ -288,5 +288,5 @@ interprete_remaining_length_268435456_test() ->
 
 %% interprete_remaining_length_test - lower limit of the second byte of the remaining length.
 interprete_packet_test() ->
-    ?assertEqual({ok, #packet_type{msgtype = publish, dup =0, qos = 0, retain = 0}, 128, <<100:1024>>},
+    ?assertEqual({ok, #type_byte{msgtype = publish, dup =0, qos = 0, retain = 0}, 128, <<100:1024>>},
 		 (pre_processor:interprete_packet(<<3:4, 0:4, 128:8, 1:8, 100:1024>>))).
