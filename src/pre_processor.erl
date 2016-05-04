e8f7607246b516e7191f4c854d46e82b311f1570
Kuldeep Sinh Chauhan <chauhan.kuldeep.sinh@gmail.com>
2016-05-03 20:02:39 -0700

    Update type specifications (#32)
    * Update type specifications

--- src/pre_processor.erl
+++ src/pre_processor.erl
@@ -8,17 +8,17 @@
 -module(pre_processor).
 -include("../include/yams_lib.hrl").
 %% API
--export([interprete_packet/1]).
+-export([interprete_fixed_header/1]).
 %% Macro
 -define(MAX_LENGTH, 268435455). % Maximum allowed length of the topic.
 
 %%===================================================================
--spec interprete_packet(packet_binary()) -> 
-			    remaining_length_ok() 
-			   |remaining_length_error()
-			   |type_byte_error().
+-spec interprete_fixed_header(packet()) -> 
+			         fixed_header_ok()
+			          | fixed_header_error()
+			          | type_byte_error().
 				
-interprete_packet(Binary) ->
+interprete_fixed_header(Binary) ->
     interprete_remaining_length(interprete_type_byte(Binary)).
 %%===================================================================
 %% Roughly speaking, interprete_type_byte/1 behaves like an interpreter-front-end.
@@ -28,58 +28,73 @@ interprete_packet(Binary) ->
 %%  1. Values of packet type and bit flags and remaining binary.
 %%  2. Or error, its reason and the binary passed to it.
 %%===================================================================
--type packet_binary()   :: <<_:16, _:_*8>>.
+-type packet() :: <<_:16, _:_*8>>.
+-type type_byte() :: #type_byte{}.
 
--type type_byte()       :: #type_byte{}.
-
--type type_byte_ok()    :: { 'ok'
-			     , type_byte()
-			     , binary()
-			   }.
+-type type_byte_ok() :: { 'ok'
+			, type_byte()
+			, binary()
+			}.
 
 -type type_byte_error() :: { 'error'
-			     , 'invalid_fb'
-			     , binary()
+			   , 'invalid_fb'
+			   , binary()
 			   }.
 
--spec interprete_type_byte(packet_binary()) -> 
+-spec interprete_type_byte(packet()) -> 
 				  type_byte_ok()
-				| type_byte_error().
+			        | type_byte_error().
 
 interprete_type_byte(<<1:4, 0:1, 0:2, 0:1, RemainingBin/binary>>) ->
     {ok, #type_byte{pkttype = connect, dup = 0, qos = 0, retain = 0}, RemainingBin};
+
 interprete_type_byte(<<2:4, 0:1, 0:2, 0:1, RemainingBin/binary>>) ->
     {ok, #type_byte{pkttype = connack, dup = 0, qos = 0, retain = 0}, RemainingBin};
+
 interprete_type_byte(<<3:4, Dup:1, 0:2, Retain:1, RemainingBin/binary>>) ->
     {ok, #type_byte{pkttype = publish, dup = Dup, qos = 0, retain = Retain}, RemainingBin};
+
 interprete_type_byte(<<3:4, Dup:1, 1:2, Retain:1, RemainingBin/binary>>) ->
     {ok, #type_byte{pkttype = publish, dup = Dup, qos = 1, retain = Retain}, RemainingBin};
+
 interprete_type_byte(<<3:4, Dup:1, 2:2, Retain:1, RemainingBin/binary>>) ->
     {ok, #type_byte{pkttype = publish, dup = Dup, qos = 2, retain = Retain}, RemainingBin};
+
 interprete_type_byte(<<4:4, 0:1, 0:2, 0:1, RemainingBin/binary>>) ->
     {ok, #type_byte{pkttype = puback, dup = 0, qos = 0, retain = 0}, RemainingBin};
+
 interprete_type_byte(<<5:4, 0:1, 0:2, 0:1, RemainingBin/binary>>) ->
     {ok, #type_byte{pkttype = pubrec, dup = 0, qos = 0, retain = 0}, RemainingBin};
+
 interprete_type_byte(<<6:4, 0:1, 1:2, 0:1, RemainingBin/binary>>) ->
     {ok, #type_byte{pkttype = pubrel, dup = 0, qos = 1, retain = 0}, RemainingBin};
+
 interprete_type_byte(<<7:4, 0:1, 0:2, 0:1, RemainingBin/binary>>) ->
     {ok, #type_byte{pkttype = pubcomp, dup = 0, qos = 0, retain = 0}, RemainingBin};
+
 interprete_type_byte(<<8:4, 0:1, 1:2, 0:1, RemainingBin/binary>>) ->
     {ok, #type_byte{pkttype = subscribe, dup = 0, qos = 1, retain = 0}, RemainingBin};
+
 interprete_type_byte(<<9:4, 0:1, 0:2, 0:1, RemainingBin/binary>>) ->
     {ok, #type_byte{pkttype = suback, dup = 0, qos = 0, retain = 0}, RemainingBin};
+
 interprete_type_byte(<<10:4, 0:1, 1:2, 0:1, RemainingBin/binary>>) ->
     {ok, #type_byte{pkttype = unsubscribe, dup = 0, qos = 1, retain = 0}, RemainingBin};
+
 interprete_type_byte(<<11:4, 0:1, 0:2, 0:1, RemainingBin/binary>>) ->
     {ok, #type_byte{pkttype = unsuback, dup = 0, qos = 0, retain = 0}, RemainingBin};
+
 interprete_type_byte(<<12:4, 0:1, 0:2, 0:1, RemainingBin/binary>>) ->
     {ok, #type_byte{pkttype = pingreq, dup = 0, qos = 0, retain = 0}, RemainingBin};
+
 interprete_type_byte(<<13:4, 0:1, 0:2, 0:1, RemainingBin/binary>>) ->
     {ok, #type_byte{pkttype = pingresp, dup = 0, qos = 0, retain = 0}, RemainingBin};
+
 interprete_type_byte(<<14:4, 0:1, 0:2, 0:1, RemainingBin/binary>>) ->
     {ok, #type_byte{pkttype = disconnect, dup = 0, qos = 0, retain = 0}, RemainingBin};
+
 interprete_type_byte(OtherBin) ->
-    {error, invalid_fb, OtherBin}.
+    {error, invalid_tb, OtherBin}.
     
 %%===================================================================
 %% This function splits the remaining binary into...
@@ -88,46 +103,50 @@ interprete_type_byte(OtherBin) ->
 %%===================================================================
 -spec interprete_remaining_length(type_byte_ok()
 			      |type_byte_error())->
-				      remaining_length_ok() 
-				     |remaining_length_error()
+				      fixed_header_ok() 
+				     |fixed_header_error()
 				     |type_byte_error().
+
 interprete_remaining_length({ok, PacketType, RemainingBin}) ->
     interprete_remaining_length({PacketType, RemainingBin}, 0, 1);
-interprete_remaining_length({error, invalid_fb, OtherBin}) ->
-    {error, invalid_fb, OtherBin}.
+
+interprete_remaining_length({error, invalid_tb, OtherBin}) ->
+    {error, invalid_tb, OtherBin}.
 
 %%==============================================
 %% internal function.
 %%==============================================
--type remaining_length()       :: 0..?MAX_LENGTH.
--type remaining_length_ok()    :: { ok
-				  , type_byte()
-				  , remaining_length()
-				  , binary()}.
--type remaining_length_error() :: { error
-				  , 'remaining_length_exceeds_max_length'
-				  , { type_byte()
-				    , binary()
-				    }
-				  }
-				 |{ error
-				  , 'remaining_length_differs_actual_length'
-				  , type_byte()
-				  , remaining_length()
-				  , binary()
-				  }.
+-type remaining_length() :: 0..?MAX_LENGTH.
+-type fixed_header_ok() :: { ok
+			   , type_byte()
+			   , remaining_length()
+			   , binary()}.
+-type fixed_header_error() :: { error
+			      , 'remaining_length_exceeds_max_length'
+			      , { type_byte()
+				, binary()
+				}
+			      }
+			     |{ error
+			      , 'remaining_length_differs_actual_length'
+			      , type_byte()
+			      , remaining_length()
+			      , binary()
+			      }.
 
 -spec interprete_remaining_length({type_byte(),binary()},non_neg_integer(),pos_integer()) -> 
-				       remaining_length_ok()
-				     | remaining_length_error().
+				       fixed_header_ok()
+				     | fixed_header_error().
 				    
 interprete_remaining_length(TypeInterpretedPacket, _RemainingLength, Multiplier)
   when (Multiplier > 128 * 128 * 128) ->
     {error, remaining_length_exceeds_max_length, TypeInterpretedPacket};
+
 %% Calculate the remaining length value:
 %% Recurse if the value of the first bit is 1.
 interprete_remaining_length({PacketType, <<1:1, Len:7, Rest/binary>>}, RemainingLength, Multiplier) ->
     interprete_remaining_length({PacketType, Rest}, RemainingLength + Len * Multiplier, Multiplier * 128);
+
 %% Calculate Value of the remaining length :
 %% Return if the value of the first bit is 0.
 interprete_remaining_length({PacketType, <<0:1, Len:7, VarLoad/binary>>}, RemainingLength, Multiplier) ->
