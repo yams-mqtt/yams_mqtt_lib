73b5cd3197cb17a5854dd6a03c22b3f462893a51
KuldeepSinh <chauhan.kuldeep.sinh@gmail.com>
2016-05-23 19:13:47 -0700

    Handle use of reserved return codes

--- src/connack.erl
+++ src/connack.erl
@@ -14,19 +14,25 @@
 %%%===================================================================
 %%% API
 %%%===================================================================
--type session_present() :: 0..1.
--type return_code() :: 0..5.
+-type session_present()     :: 0..1.
+-type return_code()         :: 0..5.
+-type illegal_return_code() :: 6..255.
+-type connack_err()         :: {error, 'illegal_return_code', illegal_return_code()}.
 -spec get_pkt({session_present(), return_code()}) ->
-	   <<_:32>>.
+	   <<_:32>> | connack_err().
 %%--------------------------------------------------------------------
-%% @doc
+%% @doc [MQTT-3.2.2-4]
+%%      If a server sends a CONNACK packet containing a non-zero return code
+%%      it MUST set Session Present to 0 [MQTT-3.2.2-4].
 %% @spec
 %% @end
 %%--------------------------------------------------------------------
 get_pkt({SessionPresent, 0}) ->
     <<2:4, 0:4, 2:8, 0:7, SessionPresent:1, 0:8>>;
-get_pkt({_SessionPresent, ReturnCode}) when ReturnCode >= 0, ReturnCode =< 5 ->
-    <<2:4, 0:4, 2:8, 0:7, 0:1, ReturnCode:8>>.
+get_pkt({_SessionPresent, ReturnCode}) when ReturnCode >= 1, ReturnCode =< 5 ->
+    <<2:4, 0:4, 2:8, 0:7, 0:1, ReturnCode:8>>;
+get_pkt({_SessionPresent, ReturnCode}) ->
+     {error, 'illegal_return_code', ReturnCode}.
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
--- test/connack_test.erl
+++ test/connack_test.erl
@@ -54,3 +54,14 @@ get_pkt_7_test() ->
     ?assertEqual(<<2:4, 0:4, 2:8, 0:7, 0:1, ReturnCode:8>>,
 		 connack:get_pkt({SessionPresent, ReturnCode})).
 
+get_pkt_8_test() ->
+    SessionPresent = 0,
+    % Generate random number between 6 and 255
+    ReturnCodes = get_list_of_randoms(1000),
+    [?assertEqual({error, 'illegal_return_code', ReturnCode}
+		 ,connack:get_pkt({SessionPresent, ReturnCode}))
+     || ReturnCode <- ReturnCodes].
+
+get_list_of_randoms(Total) ->
+    [6 + random:uniform(255 - 6) || _ <- lists:seq(1, Total)].
+ 
